a5bb21c95b5d1e6fc663bb19ea4a53f75c14d4a8
Ismael López <lc.ismael@hotmail.com>
2020-06-07 17:43:41 -0500

    :sparkles: Added new insights

--- app/main.py
+++ app/main.py
@@ -7,14 +7,16 @@ from PyQt5 import QtCore, QtWidgets
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
 from os import listdir
+from collections import Counter
+# from nltk.corpus import stopwords
+# from nltk import download
+# download('stopwords')
 
 import pandas as pd
 import json
 import math
 import datetime
 import matplotlib.pyplot as plt
-
-
 
 class MplCanvas(FigureCanvasQTAgg):
     def __init__(self, parent=None, width=5, height=4, dpi=100):
@@ -122,14 +124,42 @@ class MainWindow(QMainWindow):
         horarios = y.sort_values(ascending=False)[:3].index
         mensaje_horarios = f"Sus horarios en donde recibe más mensajes son a las {horarios[0]}hrs, las {horarios[1]}hrs y las {horarios[2]}hrs."
 
-        print(mensaje_horarios)
-
         # ¿Quienes son los clientes más activos?
         client_most_messages = client_messages.groupby(client_messages.sender_name).count().sort_values("content", ascending=False)[:3].index
         most_active_client_message = f"Sus top tres clientes más activos son 1.{client_most_messages[0]}, 2.{client_most_messages[1]}, y 3.{client_most_messages[2]}"
 
+        # ¿Cuál es el tiempo promedio que tardamos en responder?
+        pyme_filter = all_chats["sender_name"].str.strip()=='A n e m o n e'
+        pyme_messages = all_chats[pyme_filter]
+        mean_response = pyme_messages.delta_mensajes_minutos.mean()
+        mensaje_respuesta_promedio = f"Usted se tarda en promedio {int(mean_response)} minutos en responder a sus clientes."
+
+        # Tiempo promedio de respuesta de clientes
+        mean_client_response = client_messages.delta_mensajes_horas.mean()
+        mensaje_respuesta_promedio_cliente = f"Sus clientes se tardan en promedio {int(mean_client_response)} horas en responderle."
+
+        # # Palabras más usadas por Pyme
+        # spanish_stopwords = stopwords.words('spanish')
+        # words = " ".join(pyme_messages["content"]).split()
+        # filtered_words = [word for word in words if word not in spanish_stopwords]
+        # pyme_top_20_palabras = list(Counter(filtered_words).most_common(20))
+
+        # # Palabras más usadas por Clientes
+        # spanish_stopwords = stopwords.words('spanish')
+        # words = " ".join(client_messages["content"]).split()
+        # filtered_words = [word for word in words if word not in spanish_stopwords]
+        # clientes_top_20_palabras = list(Counter(filtered_words).most_common(20))
+
+        self.ui.lblInfo1.setText("Horarios con más chats")
         self.ui.lbl1.setText(mensaje_horarios)
+        self.ui.lblInfo2.setText("¿Quienes son los clientes más activos?")
         self.ui.lbl2.setText(most_active_client_message)
+        self.ui.lblInfo3.setText("¿Cuál es el tiempo promedio que tardamos en responder?")
+        self.ui.lbl3.setText(mensaje_respuesta_promedio)
+        self.ui.lblInfo4.setText("Tiempo promedio de respuesta de clientes")
+        self.ui.lbl4.setText(mensaje_respuesta_promedio_cliente)
+        # self.ui.lbl5.setText(pyme_top_20_palabras)
+        # self.ui.lbl6.setText(clientes_top_20_palabras)
 
 if __name__=="__main__":
     app = QApplication(sys.argv)
